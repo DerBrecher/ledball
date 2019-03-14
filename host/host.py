04aaa13cd06e5142f212f3faaf2d9d4ce50f936f
Robin Kremer <Robin.Kremer@iotos.io>
2019-03-14 12:27:15 -0700

    flask api first steps

--- host/host.py
+++ host/host.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 
+ledState = [
+    {
+     'state': True,
+     'effectNo': 13,
+     'testString': u'This is a Test!'
+    }
+]
+
+
 @app.route('/')
-def hello_world():
+def helloWorld():
     return "Hello World6!"
+
+@app.route('/api/ledstate', methods=['GET'])
+def returnLedState():
+    return jsonify({'ledState': ledState})
+
+@app.route('/api/newdevice', methods=['POST'])
+def addNewDevice():
+    if not request.json or not 'mac' in request.json:
+        print("No json request!")
+        abort(400)
+    print(request.json['mac'])
+    return jsonify({'State':"Okay"}),200
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
